be079875e438e2cca4d608bcfd0ad73d8f79068d
Khaled Abdulrahman :) <108235303+Khaled-Sa1em@users.noreply.github.com>
2024-12-13 04:17:09 +0200

    updates add QueryStringHelper add pagger

---
+++ CleanBlog.Core/Extensions/HTMLHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+
+namespace CleanBlog.Core.Extensions
+{
+    public static class HTMLHelperExtensions
+    {
+        public static IHtmlString GetGridHtml(this HtmlHelper htmlHelper, IPublishedContent contentItem, string framewrok, string propertyAlias, bool fluidImages)
+        {
+            if (fluidImages)
+            {
+                return htmlHelper.Raw(htmlHelper.GetGridHtml(contentItem, framewrok, propertyAlias).ToString().Replace("<img", "<img class=\"img-fluid\" "));
+            }
+            else
+            {
+                return htmlHelper.GetGridHtml(contentItem, framewrok, propertyAlias);
+            }
+        }
+    }
+}
---
+++ CleanBlog.Core/Helpers/QueryStringHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CleanBlog.Core.Helpers
+{
+    public static class QueryStringHelpers
+    {
+        public static int GetIntFromQueryString(HttpRequestBase reqest, string key, int fallbackValue = 0)
+        {
+            var stringValue = reqest.QueryString[key];
+            if (!string.IsNullOrWhiteSpace(stringValue) && int.TryParse(stringValue, out var numaricValue))
+            {
+                return numaricValue;
+            }
+            return fallbackValue;
+        }
+    }
+}
